fe7ebd41a5bc840c3d574a9ccb35279f8ac49abf
Pratik Somaiya <pratik.somaiya2000@gmail.com>
2024-09-24 01:45:36 +0100

    Parameterised robustness

--- rai_metric/robustness.py
+++ rai_metric/robustness.py
@@ -1,7 +1,47 @@
+import os
 import random
 import cv2
+import yaml
 import numpy as np
 from scipy.spatial import Delaunay
+
+def check_default_values(config, **kwargs):
+    """
+    If any values are None, update them with values from the yaml config. If not None,
+    keep the current value. The keys in kwargs should match keys in the config.
+    """
+    updated_values = {}
+    for key, value in kwargs.items():
+        if value is None:
+            config_value = get_config_value(config, key)
+            updated_values[key] = config_value
+        else:
+            updated_values[key] = value
+    return updated_values.values()
+
+def get_config_value(config, key):
+    """
+    Navigate the config dict and return the value for the key
+    """
+    for k, v in config.items():
+        # For nested dict
+        if isinstance(v, dict):
+            if key in v:
+                return v[key]
+        # If the value is not dict
+        else:
+            if key in config:
+                return config[key]
+    raise KeyError(f"Key '{key}' not found in the config.")
+
+def sort_vertices(vertices):
+    """
+    Sort vertices in clockwise order
+    """
+    centre_x = sum([v[0] for v in vertices]) / len(vertices)
+    centre_y = sum([v[1] for v in vertices]) / len(vertices)
+    return sorted(vertices, key=lambda v: np.arctan2(v[1] - centre_y, v[0] - centre_x))
+
 
 class Robustness:
     """
@@ -9,6 +49,12 @@ class Robustness:
     """
     def __init__(self, sensors):
         self.sensors = sensors
+
+        # load parameters from the config file
+        rai_path = os.environ.get('RAI_LEADERBOARD_ROOT')
+        with open(f'{rai_path}/config/robustness.yaml','r') as f:
+            self.config = yaml.safe_load(f)
+
         # default parameters for lidar from agent_wrapper.py.
         # TODO: These parameters are not directly accessible in sensors
         # but can we obtain them from the world once the scenario is loaded for
@@ -17,19 +63,20 @@ class Robustness:
         self._UPPER_FOV = 10
         self._LOWER_FOV = -30
         self._NUM_OF_CHANNELS = 64
+
         # 5 points, including the origin, defines the occlusion shape.
         # spherical (r, theta, phi)
-        self.points_in_spherical = np.array([[self._LIDAR_RANGE,50,125],
-                                            [self._LIDAR_RANGE,50,80],
-                                            [self._LIDAR_RANGE,-50,125],
-                                            [self._LIDAR_RANGE,-50,80],
+        theta_0, theta_1 = self.config['lidar']['theta']
+        phi_0, phi_1 = self.config['lidar']['phi']
+        self.points_in_spherical = np.array([[self._LIDAR_RANGE,theta_0,phi_0],
+                                            [self._LIDAR_RANGE,theta_0,phi_1],
+                                            [self._LIDAR_RANGE,theta_1,phi_0],
+                                            [self._LIDAR_RANGE,theta_1,phi_1],
                                             [0,0,0]])
         self.occlusion_hull = None
         self.lidar_occlusion_init = False
         # channel range to remove for internal noise
-        channel_range_to_remove = np.array([[0,20],
-                                            [21,40],
-                                            [42,64]])
+        channel_range_to_remove = np.array(self.config['lidar']['channel_range_to_remove'])
         self.angle_range_to_remove = self.channels_to_angle(channel_range_to_remove)
 
     def channels_to_angle(self, channel_range_to_remove, in_radians=True):
@@ -46,26 +93,23 @@ class Robustness:
             return np.radians(angle_range_to_remove)
         return angle_range_to_remove
 
-    def add_salt_and_pepper_noise(self, sensor_data, sensor_info, probability=0.8):
+    def add_salt_and_pepper_noise(self, sensor_data, sensor_info, probability=None):
         """
         Add salt and pepper noise with some given probability.
         """
         if  sensor_info:
             if sensor_info['type'] == 'camera':
+                probability, = check_default_values(self.config, probability=probability)
+
                 # Generate random noise with the same shape as the input image
                 sensor_data = sensor_data[1][:, :, :3]
-                #height, width, channel = sensor_data.shape
 
                 # Generate random noise mask
-
                 black = np.array([0, 0, 0], dtype='uint8')
                 white = np.array([255, 255, 255], dtype='uint8')
                 probs = np.random.random(sensor_data.shape[:2])
                 sensor_data[probs < (probability / 2)] = black
                 sensor_data[probs > 1 - (probability / 2)] = white
-                # Add salt and pepper noise
-
-                #sensor_data[mask] = np.where(np.random.random(mask.sum()) < 0.5, 0, 255)
 
             elif sensor_info['type'] == 'lidar':
                 # Remove selected lidar channels
@@ -107,45 +151,36 @@ class Robustness:
         norm[norm == 0] = 1.0
         return vector / norm
 
-    def add_occlussion_noise(self, sensor_data, sensor_info, num_vertices=4):
+    def add_occlussion_noise(self, sensor_data, sensor_info, num_vertices=None, random_seed=None):
         """
         Create polygon shape occlusion over camera
         """
         # Create an empty mask array
         if  sensor_info:
             if sensor_info['type'] == 'camera':
+                # Get default values from yaml if params are None
+                num_vertices, random_seed = check_default_values(self.config, num_vertices=num_vertices,
+                                                                 random_seed=random_seed)
                 # Generate random noise with the same shape as the input image
                 sensor_data = sensor_data[1][:, :, :3]
                 height, width, _ = sensor_data.shape
                 mask = np.zeros(sensor_data.shape[:2], dtype=np.uint8)
 
                 # Generate random polygon vertices
-                # Define the relative size and position of the rectangle
-                # Here, we set it to be 50% of the image width and 30% of the image height
-                rect_width = int(0.6 * width)
-                rect_height = int(0.4 * height)
+                random.seed(random_seed)
 
-                # Define the top-left, bottom right and bottom middle corner coordinates of the shape
-                # Here, we set it to be at 30% from the left and 20% from the top of the image
-                top_left_x = int(0.3 * width)
-                top_left_y = int(0.2 * height)
-                bottom_right_x = min(top_left_x + rect_width, width)
-                bottom_right_y =  min(top_left_y + rect_height, height)
+                # Generate unique vertices
+                vertices = set()
+                while len(vertices) < num_vertices:
+                    x = random.randint(0, width - 1)
+                    y = random.randint(0, height - 1)
+                    vertices.add((x, y))
 
-                #We add an additional vertice around the middle bottom area to make it a pentagon
-                mid_bottom_x = top_left_x + int(rect_width * 0.3)
-                mid_bottom_y = bottom_right_y + int((height - bottom_right_y)/2)
+                # Sort vertices in clockwise order
+                vertices_sorted = sort_vertices(list(vertices))
 
-                # Calculate the coordinates of the five vertices of the pentagon
-                top_left = (top_left_x, top_left_y)
-                top_right = (top_left_x + rect_width, top_left_y)
-                bottom_left = (top_left_x, top_left_y + rect_height)
-                bottom_right = (bottom_right_x, bottom_right_y)
-                middle_bottom = (mid_bottom_x, mid_bottom_y)
-                vertices = [top_left, top_right, bottom_right, middle_bottom, bottom_left]
-                
                 # Create a polygon mask using fillPoly
-                cv2.fillPoly(mask, [np.array(vertices)], 255)
+                cv2.fillPoly(mask, [np.array(vertices_sorted)], 255)
                 # self.display_image(mask)
                 # Invert the mask
                 inverted_mask = cv2.bitwise_not(mask)
@@ -227,23 +262,24 @@ class Robustness:
         cropped_data = sensor_data[points_removal,:]
         return cropped_data
     
-    def add_random_noise(self, sensor_data, sensor_info, noise_level=0.001):
+    def add_random_noise(self, sensor_data, sensor_info):
         """
         Add random noise to GPS readings or IMU readings depending
         on the type of sensor parameter passed in
         """
         # Generate random noise values within the specified noise_level
         if sensor_info['type'] == 'gnss':
-            return self.noise_gnss(sensor_data[1], noise_level=0.0002)
+            return self.noise_gnss(sensor_data[1])
         elif sensor_info['type'] == 'imu':
-            return self.noise_imu(sensor_data[1], acc_noise_lvl=5, compass_noise_lvl=0.0001, gyroscope_noise_lvl=0.0001)
+            return self.noise_imu(sensor_data[1])
         elif sensor_info['type'] == 'speedometer':
-            return self.noise_speedometer(sensor_data[1], speed_noise_lvl=5)
+            return self.noise_speedometer(sensor_data[1])
     
-    def noise_gnss(self, sensor_data, noise_level=0.0005):
+    def noise_gnss(self, sensor_data, noise_level=None):
         """
         Use the same level of random noise for latitude, longitude, and altitude
         """
+        noise_level, = check_default_values(self.config, noise_level=noise_level)
         lat_noise = random.uniform(-noise_level, noise_level)
         lon_noise = random.uniform(-noise_level, noise_level)
         alt_noise = random.uniform(-noise_level, noise_level)
@@ -252,10 +288,14 @@ class Robustness:
         sensor_data +=  np.array([lat_noise, lon_noise, alt_noise])
         return sensor_data
     
-    def noise_imu(self, sensor_data, acc_noise_lvl=0.5, compass_noise_lvl=0.0001, gyroscope_noise_lvl=0.0001):
+    def noise_imu(self, sensor_data, acc_noise_lvl=None, compass_noise_lvl=None, gyroscope_noise_lvl=None):
         """
         Set levels for random noise for acceleration, compass, and gyroscope
         """
+        noise_levels = check_default_values(self.config, acc_noise_lvl=acc_noise_lvl,
+                                                 compass_noise_lvl=compass_noise_lvl,
+                                                 gyroscope_noise_lvl=gyroscope_noise_lvl)
+        acc_noise_lvl, compass_noise_lvl, gyroscope_noise_lvl = noise_levels
         acc_noise = random.uniform(-acc_noise_lvl, acc_noise_lvl)
         compass_noise = random.uniform(-compass_noise_lvl, compass_noise_lvl)
         gyroscope_noise = random.uniform(-gyroscope_noise_lvl, gyroscope_noise_lvl)
@@ -266,10 +306,11 @@ class Robustness:
         sensor_data[4:] += gyroscope_noise
         return sensor_data
 
-    def noise_speedometer(self, sensor_data, speed_noise_lvl=0.5):
+    def noise_speedometer(self, sensor_data, speed_noise_lvl=None):
         """
         Set level for random noise for speedometer
         """
+        speed_noise_lvl, = check_default_values(self.config, speed_noise_lvl=speed_noise_lvl)
         speed_noise = random.uniform(-speed_noise_lvl, speed_noise_lvl)
 
         # Add noise to speedometer reading
